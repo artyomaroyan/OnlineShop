0b794b649fb4269e820e482286ce618be1ec1035
Artyom <aroyanart98@gmail.com>
2026-05-07 01:14:36 +0400

    fix: user-service. - fix `UserEntity` class for new users that DB understand it is new user and `INSERT` instead of `UPDATE`. - fix `BaseMapper<>` class to not return `Mono.Empty()`.

--- services/user/src/main/java/am/online/shop/user/model/UserEntity.java
+++ services/user/src/main/java/am/online/shop/user/model/UserEntity.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serializable;
@@ -22,7 +24,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users", schema = "users")
-public class UserEntity implements Serializable {
+public class UserEntity implements Serializable, Persistable<UUID> {
     @Id
     private UUID id;
     private String username;
@@ -31,4 +33,13 @@ public class UserEntity implements Serializable {
     private Set<Role> roles;
     private LocalDateTime createdAt;
     private boolean active;
+
+    @Transient
+    @Builder.Default
+    private boolean isNew = true;
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
 }
